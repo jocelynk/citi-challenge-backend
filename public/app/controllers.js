8b7b0a0f0de5a1dfa4471b2bd119520b11322a2e
Sasinda <sp2335@cornell.edu>
2015-11-19 19:07:06 -0500

    Merged UI changes from Josh

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -145,14 +145,14 @@ define(function () {
 
     controllers.intercept = function (event, next, current, $rootScope, $location) {
 
-/*
+
         if (next.loginRequired) {
             if (!$rootScope.currentUser) {
                 var msg = null;
                 var nextPath = '/#/' + $location.path()
                 $location.path('login/').search({'redirect': nextPath, 'message': msg});
             }
-        }*/
+        }
     }
 
     return controllers;
